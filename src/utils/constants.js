969f11b48b466cfdc78c5e28edc6b2f4c270615c
amy stout <asbilly92@gmail.com>
2022-05-05 13:27:57 -0400

    fix: :bug: added localhost link to env variable and edited the _faqs file again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,14 @@
 export const env = {
+  SITE_URL: import.meta.env.VITE_SITE_URL="http://localhost:3000"
+}
+
+
+
+
+//export VITE_SITE_URL=http://localhost:3000
+
+/* export const env = {
     SITE_URL: import.meta.env.VITE_SITE_URL
-  }
+  } */
+
+//export VITE_SITE_URL=http://localhost:3000
