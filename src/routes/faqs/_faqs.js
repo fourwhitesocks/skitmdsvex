603b73ef3998d4979be8dcb62e1750c7d1483c71
amy stout <asbilly92@gmail.com>
2022-05-05 08:25:39 -0400

    refactor: :construction: changed the _faqs export

--- src/routes/faqs/_faqs.js
+++ src/routes/faqs/_faqs.js
@@ -1,11 +1,27 @@
 // reference: https://githubmemory.com/repo/pngwn/MDsveX/issues/294
 
-//some sort of export here for the Faq?
+const modules = import.meta.globEager("../../content/faqs/*.md");
 
 
-  const modules = import.meta.globEager("../../content/faqs/*.md");
   
-  export const faqs = Object.entries(modules).map(([module]) => {
+export const faqs = Object.entries(modules).map(([path, module]) => {
+console.log(module.default)
+  const { html } = module.default.render();
+  return {
+    html,
+    ...module.metadata,
+  };
+
+ 
+});
+ 
+
+//old code before discord Mark H helped me
+/* 
+
+const modules = import.meta.globEager("../../content/faqs/*.md");
+  
+  export const faqs = Object.entries(modules).map(([path, module]) => {
     const { metadata } = module;
     const { html } = module.default.render();
     return {
@@ -16,5 +32,8 @@
    
   });
 
- 
+
+
+
+*/
 
