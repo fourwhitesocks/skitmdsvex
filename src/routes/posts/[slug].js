04190d69bb488f6e97b2bdb7fd6dd823589c8d00
amy stout <asbilly92@gmail.com>
2022-06-01 22:08:39 -0400

    refactor: :construction: trying to fix the post slug

--- src/routes/posts/[slug].js
+++ src/routes/posts/[slug].js
@@ -1,21 +1,18 @@
 
 import { query } from '$lib/data/db'
 
-
 export async function get() {
-//look at https://graphcms.com/docs/api-reference/content-api/queries#fetching-a-single-entry
- const  post  = await query(`
+
+ const  post  =  await query(` 
      
- 
-    ($slug: String!) 
-     
-    
-    post(where: { slug: $slug }) {
-      excerpt
-      title
-      slug
-    }
+  post($slug: String!) {
+  (where:{ slug:$slug }){
+    title
+    excerpt
+  }
 }
+
+
 
   `)
 
--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,11 +1,6 @@
 <script>
 	export let post;
+	//console.log(post);
 </script>
 
-<h1>Single post here</h1>
-
-{#each post.post as { title, excerpt, slug }}
-	<h2>{title}</h2>
-	<p>{excerpt}</p>
-	<p>{slug}</p>
-{/each}
+<h1>{post.title}</h1>
