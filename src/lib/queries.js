5b2fadff9f6bde0343a41d9f470290fb0f09527d
amy stout <asbilly92@gmail.com>
2022-06-23 15:17:22 -0400

    feat: :sparkles: added marked for parsing graphCMS markdown fields and fixed the dogs route

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -25,6 +25,7 @@
           breed
           slug
           excerpt
+          info  
         
       }
       
--- src/routes/dogs/index.svelte
+++ src/routes/dogs/index.svelte
@@ -1,14 +1,13 @@
 <script>
+	import { marked } from 'marked';
 	export let dogs;
 </script>
 
 <h1>Dogs Archive</h1>
 
-{#each dogs.dogs as { breed, excerpt }}
-	<!-- <a href="/dogs/{slug}"> 
-		<h2>{breed}</h2>
-	</a>-->
+{#each dogs.dogs as { breed, excerpt, info }}
 	<h2>{breed}</h2>
 	<p>{excerpt}</p>
-	<!-- <img src={image.url} alt="Girl in a jacket" /> -->
+
+	{@html marked.parse(info)}
 {/each}
