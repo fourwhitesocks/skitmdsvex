f48392ec61d6806746aeb4422f161fea1faab335
amy stout <asbilly92@gmail.com>
2022-06-28 15:12:48 -0400

    feat: :sparkles: trying to figure out the rich text editor

---
+++ src/routes/dogs/content-example.js
@@ -0,0 +1,27 @@
+
+
+export const content = {
+
+    children: [
+        {
+          type: 'heading-two',
+          children: [{ text: '' }],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            { text: ' ' },
+            { bold: true, text: '' },
+            { text: ' ' },
+            { text: '', underline: true },
+            { text: ' ' },
+            { text: '', italic: true },
+            { text: ' ' },
+            { bold: true, text: '' },
+            { text: ' ' },
+          ],
+        }
+    ]
+
+}
+ 
--- src/routes/dogs/index.svelte
+++ src/routes/dogs/index.svelte
@@ -1,5 +1,20 @@
 <script>
 	import { marked } from 'marked';
+	import { astToHtmlString } from '@graphcms/rich-text-html-renderer';
+	import { content } from './content-example';
+
+	const rich_text = astToHtmlString({
+		content,
+
+		renderers: {
+			bold: (props) => `<strong>${props.children}</strong>`,
+			Asset: {
+				application: () => `<div><p>Asset</p></div>`,
+				text: () => `<div><p>text plain</p></div>`
+			}
+		}
+	});
+
 	export let dogs;
 </script>
 
